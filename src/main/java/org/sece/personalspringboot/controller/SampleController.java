2a2b0b1b8b35da65178e7ad715a1bd656dd27524
sece <sece.mihai13@gmail.com>
2026-08-29 10:17:58 +0300

    first commit

---
+++ src/main/java/org/sece/personalspringboot/controller/SampleController.java
@@ -0,0 +1,13 @@
+package org.sece.personalspringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "Hello from SampleController";
+    }
+}
